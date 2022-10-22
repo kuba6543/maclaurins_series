ce4848198ec5011140e07f5ac4ff0a7f107f588b
kuba6543 <berta12389@wp.pl>
2022-10-22 13:40:49 +0200

    Programme working properly + English translation of functions and commentaries

--- maclaurin.c
+++ maclaurin.c
@@ -1,56 +1,52 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h> // potrzebne biblioteki
+#include <stdlib.h> // required libraries
 
-float x_start; // wartosc poczatkowa x
-float delta_x; // krok powiekszania x
-unsigned short k; // licznosc wykonywania operacji powiekszenia x_start o delta_x 
-unsigned short n; // liczba elementow szeregu potegowego Maclaurina
-int factorial(int n) // funkcja liczaca silnie 
+float x_start; // starting value of x
+float delta_x; // step of x
+unsigned short k; // quantity of steps 
+unsigned short n; // quantity of Maclaurin's Series' elements
+int factorial(int n) // function calculating factorial, used in the Maclaurin's/Taylor's series
 {
-    int sum=1; // zdefiniowanie w wypadku gdy trzeba policzyć 0!
+    int sum=1; // defining function in case of 0!
     for(n;n>0;n--)
     {
         sum *= n;
     }
     return sum;
 }
-float maclaurin_sin(float x,unsigned short n) // funkcja liczaca aproksymacje funkcji sin(x) w szeregu Maclaurina
+float maclaurin_sin(float x,unsigned short n) // function used to approximate sin(x) using Maclaurin's series
 {
-    float sum=0; // zmienna lokalna do sumowania i przedstawiania wyniku
-    float licznik; // równanie licznika szeregu
+    float sum=0; // sum of series' steps
     for(unsigned short l=0;l<n+1;l++)
     {
-        licznik=powf(-1, l) * powf(x, 2l+1); // przepisanie równania do jednej z miennej w celu uniknięcia błędów
-        sum+= licznik / factorial(2l+1);
+        sum+= (powf(-1, l) * powf(x, 2*l+1)) / factorial(2*l+1); //
     }
     return sum;
 }
-float maclaurin_cos(float x,unsigned short n) // funkcja liczaca aproksymacje funkcji cos(x) w szeregu Maclaurina
+float maclaurin_cos(float x,unsigned short n) // function used to approximate cos(x) using Maclaurin's series
 {
-    float sum=0; // zmienna lokalna do sumowania i przedstawiania wyniku
-    float licznik; // równanie licznika szeregu
+     float sum=0; // sum of series' steps
     for(unsigned short l=0;l<n+1;l++)
     {
-        licznik=powf(-1, l) * powf(x, 2l+1); // przepisanie równania do jednej z miennej w celu uniknięcia błędów
-        sum+= licznik / factorial(2l);
+        sum+= (powf(-1, l) * powf(x, 2*l)) / factorial(2*l);
     }
     return sum;
 }
 int main() 
 {
-    printf("Wprowadz wartosc poczatkowa:\n");
+    printf("Enter starting value:\n");
     scanf("%f",&x_start);
-    printf("Wprowadz wartosc kroku powiekszania:\n");
+    printf("Enter value of step:\n");
     scanf("%f",&delta_x);
-    printf("Wprowadz ilosc krokow powiekszenia funkcji:\n");
+    printf("Enter quantity of steps:\n");
     scanf("%hd",&k);
-    printf("Wprowadz ilosc elementow szeregu potegowego:\n");
+    printf("Enter quantity of Maclaurin's Series' elements:\n");
     scanf("%hd",&n);
     for(unsigned short i=0;i<k+1;i++)
     {
-        printf("Wartosc funkcji sin(x) dla %f wynosi: %f \n", x_start + i*delta_x, maclaurin_sin(x_start + i*delta_x, n));
-        printf("Wartosc funkcji cos(x) dla %f wynosi: %f \n", x_start + i*delta_x, maclaurin_cos(x_start + i*delta_x, n));
+        printf("Value of function sin(x) for x = %f is: %f \n", x_start + i*delta_x, maclaurin_sin(x_start + i*delta_x, n));
+        printf("Value of function cos(x) for x = %f is: %f \n", x_start + i*delta_x, maclaurin_cos(x_start + i*delta_x, n));
     }
     return 0;
 }
